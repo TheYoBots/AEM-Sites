76eba3baed35166d3fc97df113ace64d3fb31ca5
Yohaan Seth Nathan <73843275+TheYoBots@users.noreply.github.com>
2025-12-14 13:33:32 +0000

    Init

---
+++ CoreJava/Day4/LoginException.java
@@ -0,0 +1,12 @@
+package Day4;
+// UnChecked are not-Mandatory      
+// Checked Mandatory
+public class LoginException extends RuntimeException // Exception
+{
+	public LoginException() {
+		System.out.println("Please Enter Valid Credentials");
+	}
+	public LoginException(String message) {
+		System.out.println(message);
+	}
+}
